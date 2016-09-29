9712f00de4d8dee280c554bb15c0ccec2b08c40f
hoipo <578344975@qq.com>
2016-09-29 15:50:58 +0800

    fixed the function to hide wrapper

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
  new PageSlider({
      pages: $('.page-wrap .page')
  });
+
+ function loaderCompleted() {
+     $("#page1 .panel").one("webkitAnimationEnd animationEnd", function () {
+        $("#page1 .panel-wrapper").tap(function() {
+            $(this).addClass("hide");
+        })
+     })
+ }
